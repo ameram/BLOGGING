e5e0859387b514745161ad0b5a3820bcd38ce331
Ameer <ameer@Ameers-MacBook-Pro.local>
2021-02-15 10:40:39 +0330

    Bug fix

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
@@ -18,7 +18,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.string(128))
+    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     @property
