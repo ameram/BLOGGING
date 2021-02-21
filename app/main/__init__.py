f912fe4dd3547c19f1a1d6f04d34769be5a08556
ameer <ameermohammadramzani@gmail.com>
2021-02-21 11:37:53 +0330

    users

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,3 +2,8 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 from . import views, errors
+
+
+@main.app_context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
